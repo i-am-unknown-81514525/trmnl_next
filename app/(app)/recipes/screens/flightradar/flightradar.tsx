0a6c89d267f394b6aa0ef014bb91b499a0de35bf
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2026-02-16 15:51:34 +0000

    Add reminder for myself how I bruteforce the fr24 API

---
+++ app/(app)/recipes/screens/flightradar/flightradar.tsx
@@ -0,0 +1,5 @@
+// https://api.flightradar24.com/common/v1/flight-playback.json?flightId=3e1f9a70&timestamp=1769798100
+// [8:38 PM]https://www.flightradar24.com/aircraft/images/?aircraft=4073A0
+// [8:39 PM]https://www.flightradar24.com/v1/search/web/find?query=EZY16CH&limit=50 8 digit id available from here, with the other flight ID: U22934
+// [8:40 PM]https://api.flightradar24.com/common/v1/flight/list.json?&fetchBy=flight&page=1&limit=25&query=U22934 use the flight id, and find the one in the correct time to get the hex id: 4073A0
+// [8:40 PM]and you can get the image
