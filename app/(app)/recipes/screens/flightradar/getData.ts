6f165274542965b90260c8d975de3cd67783bdf4
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2026-02-16 18:35:56 +0000

    Group lat,long to Location

--- app/(app)/recipes/screens/flightradar/getData.ts
+++ app/(app)/recipes/screens/flightradar/getData.ts
@@ -1,33 +1,40 @@
-export interface ForeignFlightData {
-    callsign: string
+export interface Location {
     lat: number,
     long: number
 }
 
+export interface ForeignFlightData {
+    callsign: string,
+    loc: Location
+}
+
 export interface Trail {
-    lat: number,
-    long: number,
+    loc: Location,
     speed: number,
     height: number
 }
 
 export interface FlightData {
     callsign: string,
-    lat: number,
-    long: number
+    loc: Location,
     speed: number,
     height: number,
+    track: number,
     trail: Trail[]
 }
 
+export interface Airport {
+    code: string,
+    loc: Location
+}
+
 type TrackingKind =
-    | { kind: 'K1' }
-    | { kind: 'K2' }
-    | { kind: 'K3'; dt: FlightData };
+    | { kind: 'static_location', location: Location }
+    | { kind: 'static_airport', airport: Airport }
+    | { kind: 'K3'; flight: FlightData };
 
 export interface DisplayData {
     tracking: TrackingKind,
-    center_lat: number,
-    center_long: number,
+    center_loc: Location,
     nearby: ForeignFlightData[]
 }
