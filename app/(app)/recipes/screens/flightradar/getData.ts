defab9b67ca383c5969fffacb50c632bf1dfde67
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2026-02-16 18:52:23 +0000

    Handle decompression

--- app/(app)/recipes/screens/flightradar/getData.ts
+++ app/(app)/recipes/screens/flightradar/getData.ts
@@ -1,3 +1,8 @@
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createZstdDecompress } from 'node:zlib';
+import { pipeline } from 'node:stream/promises';
+import { Decompressor } from 'zstd-wasm';
+
 export interface Location {
     lat: number,
     long: number
@@ -37,4 +42,16 @@ export interface DisplayData {
     tracking: TrackingKind,
     center_loc: Location,
     nearby: ForeignFlightData[]
+}
+
+async function getFlightInZone(bottom_left: Location, top_right: Location) : Promise<ForeignFlightData[]> {
+    const response = await fetch(`https://globe.adsbexchange.com/re-api/?binCraft&zstd&box=${bottom_left.lat},${top_right.lat},${bottom_left.long},${top_right.long}`)
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const body = await response.bytes();
+    const decompressor = new Decompressor();
+    await decompressor.init();
+    const result = decompressor.decompress(body);
+    throw new Error("Not implemented yet");
 }
