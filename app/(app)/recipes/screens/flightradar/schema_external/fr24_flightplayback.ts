a86c0078e69116540fc167cb512f346e2854923a
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2026-02-17 19:20:58 +0000

    Continue with schema

--- app/(app)/recipes/screens/flightradar/schema_external/fr24_flightplayback.ts
+++ app/(app)/recipes/screens/flightradar/schema_external/fr24_flightplayback.ts
@@ -1,6 +1,6 @@
 export interface FlightIdentification {
     id: string,
-    number: {default: string},
+    number: {default: string | null},
     callsign: string
 }
 
@@ -8,18 +8,43 @@ export interface FlightStatusData {
     status: {
         text: "estimated" | "landed" | "scheduled" | string,
         type: "arrival" | string,
-        color: "green" | "gray" | "yellow" | string | null,
+        color: "green" | "gray" | "yellow" | string | null, // default: red if null?
         diverted: null
+    },
+    eventTime: {
+        utc: number | null, // For est arrival
+        local: number | null
     }
 }
 
 export interface FlightStatus {
     live: boolean,
     text: "Scheduled" | string,
-    icon: "green" | "red" | "yellow" | string | null,
+    icon: "green" | "red" | "yellow" | string | null, // default: red if null? // nvm it can still be yellow when null
     estimated: null,
     ambiguous: boolean,
-    generic:
+    generic: FlightStatusData
+}
+
+export interface AirCraftModel {
+    code: string,
+    text: string
+}
+
+export interface AirCraftIdentification {
+    model: string,
+    registration: string,
+    serialNo: string | number | null | any,
+    age: {availability: boolean},
+}
+
+export interface Airline {
+    name: string,
+    code: {
+        iata: string,
+        icao: string
+    },
+    short?: string
 }
 
 export interface FR24PlaybackResult {
@@ -29,10 +54,22 @@ export interface FR24PlaybackResult {
             altitudeFiltered: boolean,
             data: {
                 flight: {
-                    identification: FlightIdentification
+                    identification?: FlightIdentification // turn out some plane show absolutely nothing
+                    status?: FlightStatus,
+                    aircraft?: {
+                        model: AirCraftModel,
+                        identification: AirCraftIdentification,
+                        availability: {serialNo: boolean, age: boolean}
+                    },
+                    owner?: Airline,
+                    airline?: Airline
                 }
                 aircraftImage: {}
             }
         }
     }
 }
+
+// Example of nothing
+// {"result":{"request":{"callback":null,"device":null,"flightId":"3E607C84","format":"json","pk":null,"timestamp":1771257600,"token":null},"response":{"data":{"flight":{"availability":{"ems":false}}}}}}
+
