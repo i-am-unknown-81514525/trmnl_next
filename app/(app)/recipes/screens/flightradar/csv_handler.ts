86a05da807e88fce5919d4b7d9e537fa4f1b5c32
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2026-02-18 18:41:55 +0000

    Add conversion function

--- app/(app)/recipes/screens/flightradar/csv_handler.ts
+++ app/(app)/recipes/screens/flightradar/csv_handler.ts
@@ -2,3 +2,14 @@ import fs from 'fs';
 import { parse } from 'csv-parse/sync';
 import Flatbush from 'flatbush';
 import {AirportExtended, Runway, Navaid} from "./schema_external/scraped_data";
+
+function toNum(v: string | undefined) : number | null {
+    if (v === undefined || v === null || v === '') return null;
+    const number = Number(v);
+    return isFinite(number) ? number : null;
+}
+
+function toBool(v: string | undefined) : boolean {
+    if (v === undefined || v === null || v === '') return false;
+    return v.toLowerCase() in ['1', 'true'];
+}
