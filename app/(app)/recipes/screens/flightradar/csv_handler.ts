f25b1663c5307fdb6087e535819207c38c57f1dd
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2026-02-18 18:44:25 +0000

    Set global variables

--- app/(app)/recipes/screens/flightradar/csv_handler.ts
+++ app/(app)/recipes/screens/flightradar/csv_handler.ts
@@ -13,3 +13,11 @@ function toBool(v: string | undefined) : boolean {
     if (v === undefined || v === null || v === '') return false;
     return v.toLowerCase() in ['1', 'true'];
 }
+
+let loaded: boolean = false;
+let airports: AirportExtended[] = [];
+let airports_idx: Flatbush | null = null;
+let navaids: Navaid[] = [];
+let navaids_idx: Flatbush | null = null;
+let runways: Runway[] = [];
+let runways_idx: Flatbush | null = null;
