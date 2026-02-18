6cb705d3d59b3f6755d96d60249d7a8cd9662fdb
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2026-02-18 18:56:31 +0000

    Add airports.csv loading

--- app/(app)/recipes/screens/flightradar/csv_handler.ts
+++ app/(app)/recipes/screens/flightradar/csv_handler.ts
@@ -21,3 +21,39 @@ let navaids: Navaid[] = [];
 let navaids_idx: Flatbush | null = null;
 let runways: Runway[] = [];
 let runways_idx: Flatbush | null = null;
+
+export function loadAllSync() : void {
+    // airports.csv
+    const airport_raw = fs.readFileSync("data/airports.csv", "utf8");
+    const airport_rows: Record<string, string>[] = parse(airport_raw, {columns: true, skip_empty_lines: true, trim: true});
+    // I gave up making more schema ahhhhhhhhhhhhh there are too many
+    for (const airport_row of airport_rows) {
+        const ident = (airport_row.ident || '').trim().toUpperCase() || null;
+        const iata = (airport_row.iata_code || '').trim().toUpperCase() || null;
+        const lat = toNum(airport_row.latitude_deg);
+        if (lat === null) {
+            continue;
+        }
+        const lon = toNum(airport_row.longitude_deg);
+        if (lon === null) {
+            continue;
+        }
+        const rec: AirportExtended = {
+            id: airport_row.id,
+            ident,
+            iata,
+            name: airport_row.name || null,
+            lat,
+            lon,
+            runways: []
+        }
+        airports.push(rec);
+    }
+    if (airports.length > 0) {
+        airports_idx = new Flatbush(airports.length);
+        for (const airport of airports) {
+            airports_idx.add(airport.lon, airport.lat, airport.lat, airport.lon);
+        }
+        airports_idx.finish();
+    }
+}
