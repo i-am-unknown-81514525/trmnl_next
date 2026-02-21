a6900f70a7dfd6b86640f752335e04d39cc35d29
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2026-02-21 17:21:49 +0000

    Complete schema for /common/v1/airport.json?code=...&plugin[]=schedule&plugin-setting[schedule][mode]=arrivals|departures&plugin-setting[schedule][timestamp]=...&limit=100&page=1

--- app/(app)/recipes/screens/flightradar/schema_external/fr24_airport_schedule.ts
+++ app/(app)/recipes/screens/flightradar/schema_external/fr24_airport_schedule.ts
@@ -1,6 +1,43 @@
+import type {
+  FlightIdentification,
+  FlightStatus,
+  AirCraftModel,
+  AirCraftIdentification,
+  Airline,
+  Airport,
+  Thumbnail,
+} from "./fr24_flightplayback";
+import type { TimeData } from "./fr24_flight_list";
+
+export interface FR24AirportListFlightResult {
+  flight: {
+    identification?: FlightIdentification; // turn out some plane show absolutely nothing
+    status?: FlightStatus;
+    aircraft?: {
+      model: AirCraftModel;
+      identification: AirCraftIdentification;
+      availability: { serialNo: boolean; age: boolean };
+    };
+    owner?: Airline | null;
+    airline?: Airline | null;
+    airport?: {
+      origin: Airport;
+      destination: Airport;
+      real: Airport | null; // diversion
+    };
+    time: TimeData;
+  };
+}
+
+export interface FR24AirlinesData {
+  codeshare: Record<string, Airline>;
+}
+
 export interface FR24FlightDataPagingWrap {
   item: { current: number; total: number; limit: number };
   page: { current: number; total: number };
+  timestamp: number;
+  data: FR24AirportListFlightResult[];
 }
 
 export interface FR24AirportData {
@@ -8,13 +45,35 @@ export interface FR24AirportData {
   arrivals?: FR24FlightDataPagingWrap;
 }
 
+export interface FR24AircraftImgData {
+  registration: string;
+  images: {
+    thumbnails: Thumbnail[];
+    medium: Thumbnail[];
+    large: Thumbnail[];
+  };
+}
+
 export interface FR24AirportResp {
   airport: { pluginData: FR24AirportData };
   airlines: FR24AirlinesData;
-  aircraftImages: FR24AircraftImgData;
+  aircraftImages: FR24AircraftImgData[];
 }
 
 export interface FR24AirportResult {
-  request: object;
-  response: object;
+  request: {
+    callback: null | any;
+    code: string | any;
+    device: null | any;
+    fleet: null | any;
+    format: "json" | any;
+    limit: number;
+    page: number;
+    pk: null | any;
+    plugin: ["schedule"] | string[];
+    "plugin-setting": { mode: "arrivals" | "departures"; timestamp: number };
+    satelliteImage: { scale: 1 | number };
+    token: number | any;
+  };
+  response: FR24AirportResp;
 }
