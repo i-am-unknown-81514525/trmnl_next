e027aeddfd8508da8b655a209737975f54ff5d2e
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2026-02-22 01:10:33 +0000

    Update possible schema

--- app/(app)/recipes/screens/flightradar/schema_external/fr24_flightplayback.ts
+++ app/(app)/recipes/screens/flightradar/schema_external/fr24_flightplayback.ts
@@ -1,154 +1,156 @@
 export interface FlightIdentification {
-	id: string;
-	number: { default: string | null };
-	callsign: string | null;
+  id: string;
+  row?: number | null;
+  number: { default: string | null };
+  callsign: string | null;
+  codeshare?: string[];
 }
 
 export interface FlightStatusData {
-	status: {
-		text: "estimated" | "landed" | "scheduled" | "diverted" | string;
-		type: "arrival" | "departure" | string;
-		color: "green" | "gray" | "yellow" | string | null; // default: red if null?
-		diverted: string | null; // Airport code
-	};
-	eventTime: {
-		utc: number | null; // For est arrival, null on divert
-		local: number | null;
-	};
+  status: {
+    text: "estimated" | "landed" | "scheduled" | "diverted" | string;
+    type: "arrival" | "departure" | string;
+    color: "green" | "gray" | "yellow" | string | null; // default: red if null?
+    diverted: string | null; // Airport code
+  };
+  eventTime: {
+    utc: number | null; // For est arrival, null on divert
+    local: number | null;
+  };
 }
 
 export interface FlightStatus {
-	live: boolean;
-	text: "Scheduled" | string;
-	icon: "green" | "red" | "yellow" | string | null; // default: red if null? // nvm it can still be yellow when null
-	// diversion show red
-	estimated: null;
-	ambiguous: boolean;
-	generic: FlightStatusData;
+  live: boolean;
+  text: "Scheduled" | string;
+  icon: "green" | "red" | "yellow" | string | null; // default: red if null? // nvm it can still be yellow when null
+  // diversion show red
+  estimated: null;
+  ambiguous: boolean;
+  generic: FlightStatusData;
 }
 
 export interface AirCraftModel {
-	code: string;
-	text: string;
+  code: string;
+  text: string;
 }
 
 export interface AirCraftIdentification {
-	model: string;
-	registration: string;
-	serialNo: string | number | null | any;
-	age: { availability: boolean };
+  model: string;
+  registration: string;
+  serialNo: string | number | null | any;
+  age: { availability: boolean };
 }
 
 export interface Airline {
-	name: string;
-	code: {
-		iata: string;
-		icao: string;
-	};
-	short?: string;
+  name: string;
+  code: {
+    iata: string;
+    icao: string;
+  };
+  short?: string;
 }
 
 export interface Country {
-	name: string;
-	code: string;
-	id: number;
+  name: string;
+  code: string;
+  id: number;
 }
 
 export interface Timezone {
-	name: string;
-	offset: number;
-	abbr: string;
-	abbrName: string;
-	isDst: boolean;
+  name: string;
+  offset: number;
+  abbr: string;
+  abbrName: string;
+  isDst: boolean;
 }
 
 export interface Airport {
-	name: string;
-	code: {
-		iata: string;
-		icao: string;
-	};
-	position: {
-		latitude: number;
-		longitude: number;
-		country: Country;
-		region: { city: string };
-		timezone: Timezone;
-	};
+  name: string;
+  code: {
+    iata: string;
+    icao: string;
+  };
+  position: {
+    latitude: number;
+    longitude: number;
+    country: Country;
+    region: { city: string };
+    timezone: Timezone;
+  };
 }
 
 export interface MedianTimeData {
-	// wtf is this
-	time: number | null;
-	delay: number | null;
-	timestamp: number | null;
+  // wtf is this
+  time: number | null;
+  delay: number | null;
+  timestamp: number | null;
 }
 
 export interface HeightData {
-	feet: number;
-	meters: number;
+  feet: number;
+  meters: number;
 }
 
 export interface SpeedData {
-	kmh: number;
-	kts: number;
-	mph: number;
+  kmh: number;
+  kts: number;
+  mph: number;
 }
 
 export interface VSpeedData {
-	fpm: number;
-	ms: number;
+  fpm: number;
+  ms: number;
 }
 
 export interface TrackEntry {
-	timestamp: number;
-	latitude: number;
-	longitude: number;
-	altitude: HeightData;
-	speed: SpeedData;
-	verticalSpeed: VSpeedData;
-	heading: number;
-	squawk: string;
-	ems: null | any;
+  timestamp: number;
+  latitude: number;
+  longitude: number;
+  altitude: HeightData;
+  speed: SpeedData;
+  verticalSpeed: VSpeedData;
+  heading: number;
+  squawk: string;
+  ems: null | any;
 }
 
 export interface Thumbnail {
-	src: string;
-	link: string;
+  src: string;
+  link: string;
 }
 
 export interface FR24PlaybackResult {
-	result: {
-		response: {
-			timestamp: number;
-			altitudeFiltered: boolean;
-			data: {
-				flight: {
-					identification?: FlightIdentification; // turn out some plane show absolutely nothing
-					status?: FlightStatus;
-					aircraft?: {
-						model: AirCraftModel;
-						identification: AirCraftIdentification;
-						availability: { serialNo: boolean; age: boolean };
-					};
-					owner?: Airline | null;
-					airline?: Airline | null;
-					airport?: {
-						origin: Airport;
-						destination: Airport;
-						real: Airport | null; // diversion
-					};
-					median?: MedianTimeData;
-					track?: TrackEntry[];
-					aircraftImage?: {
-						sideview: string;
-						thumbnails: Thumbnail[];
-					};
-					availability: { ems: boolean };
-				};
-			};
-		};
-	};
+  result: {
+    response: {
+      timestamp: number;
+      altitudeFiltered: boolean;
+      data: {
+        flight: {
+          identification?: FlightIdentification; // turn out some plane show absolutely nothing
+          status?: FlightStatus;
+          aircraft?: {
+            model: AirCraftModel;
+            identification: AirCraftIdentification;
+            availability: { serialNo: boolean; age: boolean };
+          };
+          owner?: Airline | null;
+          airline?: Airline | null;
+          airport?: {
+            origin: Airport;
+            destination: Airport;
+            real: Airport | null; // diversion
+          };
+          median?: MedianTimeData;
+          track?: TrackEntry[];
+          aircraftImage?: {
+            sideview: string;
+            thumbnails: Thumbnail[];
+          };
+          availability: { ems: boolean };
+        };
+      };
+    };
+  };
 }
 
 // Example of nothing
